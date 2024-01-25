54ff3756924d5f5dd3ffbea2bcaf2b6bf84e6a2d
Marianllns <mariana.alonllns@outlook.com>
2024-01-24 21:58:26 -0600

    final commit

---
+++ 04_conexion_db.py
@@ -0,0 +1,36 @@
+import psycopg2
+
+# crear objeto de conexión
+
+con = psycopg2.connect(
+    host="ec2-50-17-37-172.compute-1.amazonaws.com",
+    database="d2a6f6ik93fsj6",
+    user="gguzman",
+    password="p394a126d55e12096219d2fe53431aff619f3d86fa36697a76f32bf57f19ced06"
+)
+
+# crear cursor
+
+cursor = con.cursor()
+
+# MySQL
+# Show tables
+
+try:
+    query = '''
+    SELECT table_name
+    FROM information_schema.tables
+    WHERE table_schema = 'public'
+    '''
+
+    cursor.execute(query)
+
+    tablas = cursor.fetchall()
+
+    for t in tablas:
+        print(t[0])
+except psycopg2.Error as e:
+    print(f'Un error en la conexion {e}')
+finally:
+    cursor.close()
+    con.close()
